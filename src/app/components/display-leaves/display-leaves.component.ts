c634bce04f3143b6e2273af3a6e203f72865fc72
Laurence Ott <lodev.diginamic@gmail.com>
2022-02-24 10:26:14 +0100

    Refactoring menu and methods

--- src/app/components/display-leaves/display-leaves.component.ts
+++ src/app/components/display-leaves/display-leaves.component.ts
@@ -26,13 +26,17 @@ export class DisplayLeavesComponent implements OnInit {
   };
 
   constructor(private leavesService: LeavesService, private snackBar: MatSnackBar) {
+    this.loadLeaves()
+  }
+
+  ngOnInit(): void {
+  }
+
+  loadLeaves(){
     this.leavesService.getLeavesByEmployee(12).subscribe({
       next: (leaves) => this.dataSource.data = leaves,
       error: (e) => console.log(e)
     });
-  }
-
-  ngOnInit(): void {
   }
 
   deleteLeave(id: number) {
@@ -40,10 +44,7 @@ export class DisplayLeavesComponent implements OnInit {
       next: (leave) => {
         this.snackBar.open('La demande de congé du ' + leave.startDate + ' au ' + leave.endDate + ' a bien été supprimée', '', this.snackBarConfig);
         // Rechargement de la liste
-        this.leavesService.getLeavesByEmployee(12).subscribe({
-          next: (leaves) => this.dataSource.data = leaves,
-          error: (e) => console.log(e)
-        });
+        this.loadLeaves();
       }
     })
   }
@@ -55,10 +56,7 @@ export class DisplayLeavesComponent implements OnInit {
     const status = row.status;
 
     // 1. Ne pas supprimer une demande inférieure à la date du jour
-
-    if (startDate < dateOfDay) {
-      flagIsDesabled = true;
-    }
+    flagIsDesabled = this.checkDateDateStartAfterCurrentDay(row.startDate);
     // 2. Ne pas supprimer une demande en attente de validation
     if (status === LeaveStatus.PENDING_VALIDATION) {
       flagIsDesabled = true;
@@ -73,11 +71,21 @@ export class DisplayLeavesComponent implements OnInit {
     const dateOfDay = DateTime.now();
 
     // 1. Ne pas modifier une demande inférieure à la date du jour - TODO à confirmer
-    if (startDate < dateOfDay) {
-      flagIsDesabled = true;
-    }
+    flagIsDesabled = this.checkDateDateStartAfterCurrentDay(row.startDate);
+
     // 2. Ne pas supprimer une demande en attente de validation ou validée
     if (status === LeaveStatus.PENDING_VALIDATION || status === LeaveStatus.VALIDATED) {
+      flagIsDesabled = true;
+    }
+    return flagIsDesabled;
+  }
+
+  checkDateDateStartAfterCurrentDay(dateStart: Date): boolean {
+    const startDate = DateTime.fromJSDate(new Date(dateStart));
+    const dateOfDay = DateTime.now();
+    let flagIsDesabled = false;
+    // 1. Ne pas modifier une demande inférieure à la date du jour - TODO à confirmer
+    if (startDate < dateOfDay) {
       flagIsDesabled = true;
     }
     return flagIsDesabled;
