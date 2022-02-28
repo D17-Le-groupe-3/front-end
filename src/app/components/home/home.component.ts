e20a0e6af8f2bb1f96b39b2446276c0a608e3bf1
Pierre-Emmanuel Vilcot <pevilcot@gmail.com>
2022-02-28 11:24:02 +0100

    removed useless test

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {UserService} from "../../services/user.service";
-import {HttpClient} from "@angular/common/http";
-import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-home',
@@ -9,27 +6,10 @@ import {environment} from "../../../environments/environment";
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  greeting: Greeting = {
-    id: "",
-    content: ""
-  };
 
-  constructor(private userService: UserService, private http: HttpClient) {
-    http.get<Greeting>(environment.backendUrl + '/test').subscribe(data => {
-      if (data)
-        this.greeting = data
-    });
-  }
+  constructor() { }
 
   ngOnInit(): void {
   }
 
-  authenticated() {
-    return this.userService.authenticated;
-  }
-}
-
-interface Greeting {
-  id: string;
-  content: string;
 }
